ff717355e375de12b94c4ce716787f614896dec3
TheScientist137 <thescientist137@outlook.es>
2025-07-25 09:12:24 +0200

    added search tracks types

--- src/types/spotify.ts
+++ src/types/spotify.ts
@@ -10,3 +10,49 @@ export interface SpotifyUser {
   email: string,
   images: UserImage[]
 }
+
+
+interface Album {
+  id: string,
+  type: string,
+  href: string,
+  uri: string,
+  name: string,
+  album_type: string,
+  total_tracks: number,
+  available_markets: string[],
+  external_urls: { spotify: string },
+  images: {
+    url: string,
+    height: number | null,
+    width: number | null,
+  }[],
+
+}
+
+interface Artist {
+  id: string,
+  type: string,
+  href: string,
+  uri: string,
+  name: string,
+  external_urls: { spotify: string }
+}
+
+export interface SpotifyTrack {
+  id: string,
+  type: string,
+  href: string,
+  uri: string,
+  name: string,
+  duration_ms: number,
+  album: Album,
+  artists: Artist[],
+  external_urls: { spotify: string }
+}
+
+export interface TracksSearchResponse {
+  tracks: {
+    items: SpotifyTrack[]
+  }
+}
