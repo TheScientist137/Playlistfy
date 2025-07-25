13cdd6698a377982a48c53f067d5aef43c6006a6
TheScientist137 <thescientist137@outlook.es>
2025-07-25 09:51:14 +0200

    Created searchTracks service function (need to test)

--- src/services/spotifyApi.ts
+++ src/services/spotifyApi.ts
@@ -1,4 +1,6 @@
-export const getUserProfile = async (token: string): Promise<any> => {
+import type { SpotifyUser, TracksSearchResponse } from "../types/spotify";
+
+export const getUserProfile = async (token: string): Promise<SpotifyUser> => {
   const res = await fetch("https://api.spotify.com/v1/me", {
     method: "GET",
     headers: { Authorization: `Bearer ${token}` }
@@ -6,4 +8,20 @@ export const getUserProfile = async (token: string): Promise<any> => {
 
   if (!res.ok) throw new Error(`User fetch failed: ${res.status}`);
   return await res.json();
+}
+
+export const searchTracks = async (token: string, query: string, limit = 20): Promise<TracksSearchResponse> => {
+  const params = new URLSearchParams({
+    q: query,
+    type: 'track',
+    limit: String(limit)
+  })
+
+  const res = await fetch(`https://api.spotify.com/v1/search?${params.toString()}`, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` }
+  });
+
+  if (!res.ok) throw new Error("Search tracks failed");
+  return res.json();
 }
