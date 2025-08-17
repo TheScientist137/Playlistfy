afe84aa0eebc1146bf5e31948f1c3c4afbfa6361
TheScientist137 <thescientist137@outlook.es>
2025-08-17 09:29:10 +0200

    created first main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router";
+import Login from "./pages/Login.tsx";
 
 import LoginButton from "./components/LoginButton.tsx";
 import UserProfile from "./components/UserProfile.tsx";
@@ -16,68 +18,56 @@ import type {
 } from "./types/spotify";
 
 import { exchangeCodeForToken } from "./services/pkceAuth";
+
 import {
   getUserProfile,
   searchTracks,
   createPlaylist,
 } from "./services/spotifyApi";
+import { Dashboard } from "./components/Dashboard.tsx";
 
 function App() {
-  const [token, setToken] = useState<string | null>(
-    localStorage.getItem("spotify_access_token"),
-  );
   const [profile, setProfile] = useState<SpotifyUser | null>(null);
   const [tracks, setTracks] = useState<SpotifyTrack[] | null>(null);
-
   const [searchLoading, setSearchLoading] = useState(false);
   const [showForm, setShowForm] = useState(false);
-
   const [playlist, setPlaylist] = useState<SpotifyPlaylist | null>(null);
   const [showSuccess, setShowSuccess] = useState(false);
 
   // Effect for authorization with token
   useEffect(() => {
-    // If already is a token => return
-    if (localStorage.getItem("spotify_access_token")) return;
+    const init = async () => {
+      // Get exchange code
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get("code");
 
-    const url = new URL(window.location.href);
-    const code = url.searchParams.get("code");
-    if (!code) return; // No code => nothing to do
+      try {
+        // Exchange code for token and clean url
+        if (code) {
+          await exchangeCodeForToken(code);
+          window.history.replaceState({}, "", "/");
+        }
 
-    // 3. Limpiamos la URL **antes** de la segunda montura
-    // El segundo render no ve "code" ya que lo hemos limpiado
-    window.history.replaceState({}, "", "/");
-
-    exchangeCodeForToken(code)
-      .then((data) => {
-        localStorage.setItem("spotify_access_token", data.access_token);
-        setToken(data.access_token);
-      })
-      .catch((error) => {
+        // Get user profile (using token on api)
+        const profileData = await getUserProfile();
+        setProfile(profileData);
+      } catch (error) {
         console.error(error);
-      });
+      }
+    };
+
+    init();
   }, []);
-
-  // Effect for user info if it is already logued in
-  useEffect(() => {
-    if (!token) return;
-
-    getUserProfile(token)
-      .then((data) => {
-        console.log(data);
-        setProfile(data);
-      })
-      .catch((error) => console.error(error));
-  }, [token]);
 
   // Search handler
   const handleSearch = async (query: string) => {
     if (!query) {
       setTracks([]);
+
       return;
     }
     try {
-      const data = await searchTracks(token!, query);
+      const data = await searchTracks(query);
       setTracks(data.tracks.items);
     } catch (error) {
       setTracks([]);
@@ -89,14 +79,9 @@ function App() {
 
   // Create playlist handler
   const handleCreatePlaylist = async (name: string, description: string) => {
-    if (!token || !profile) return;
+    if (!profile) return;
     try {
-      const newPlaylist = await createPlaylist(
-        token,
-        profile.id,
-        name,
-        description,
-      );
+      const newPlaylist = await createPlaylist(profile.id, name, description);
       setPlaylist(newPlaylist);
       setShowSuccess(true);
       // Hide success message after 3 seconds
@@ -111,38 +96,18 @@ function App() {
 
   const handleLogOut = () => {
     localStorage.removeItem("spotify_access_token");
-    setToken(null);
     setProfile(null);
     setTracks([]);
   };
 
-  if (!token) return <LoginButton />;
-  if (!profile) return <p>Loading profile</p>;
+  if (!profile) return <LoginButton />;
 
   return (
-    <main>
-      <h1>Welcome to Playlistfy</h1>
-      <UserProfile profile={profile} onLogout={handleLogOut} />
-      <SearchBar onSearch={handleSearch} />
-      <Tracklist tracks={tracks} />
-
-      <PlaylistList token={token} />
-
-      {!showForm && !showSuccess && (
-        <button onClick={() => setShowForm(true)} style={{ marginTop: 16 }}>
-          Create new playlist
-        </button>
-      )}
-
-      {showForm && (
-        <div>
-          <CreatePlaylistForm />
-          <button onClick={() => setShowForm(false)}>close</button>
-        </div>
-      )}
-
-      {showSuccess && <p>Playlist creada ✅</p>}
-    </main>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/callback" element={<Callback />} />
+    </Routes>
   );
 }
 
