959bdda3ce2e7be652ef3ed0fb8db50afac1c293
TheScientist137 <thescientist137@outlook.es>
2025-08-25 09:16:53 +0200

    deleted files

--- src/components/CreatePlaylistForm.tsx
+++
@@ -1,9 +0,0 @@
-export default function CreatePlaylistForm() {
-  return (
-    <form>
-      <input placeholder="Playlist name" />
-      <input placeholder="Playlist description" />
-      <button type="submit">create</button>
-    </form>
-  );
-}
