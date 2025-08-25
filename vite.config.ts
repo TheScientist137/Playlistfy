56155d7c8bb0e7f09095dd9e8f555f784a6d9565
TheScientist137 <thescientist137@outlook.es>
2025-08-25 09:11:15 +0200

    added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {             // Opciones del servidor (en desarrollo)
-    host: '127.0.0.1',  // escucha en la IP de loop-back (evita problemas con Spotify)
-    port: 5173          // puerto fijo para que la redirect_uri coincida
-  }
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    // Opciones del servidor (en desarrollo)
+    host: "127.0.0.1", // escucha en la IP de loop-back (evita problemas con Spotify)
+    port: 5173, // puerto fijo para que la redirect_uri coincida
+  },
+});
