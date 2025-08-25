2bf5aa99e33db31e5bafea9bd73598796fc602b8
TheScientist137 <thescientist137@outlook.es>
2025-08-25 09:11:57 +0200

    callback between login and dashboard

---
+++ src/components/RequireAuth.tsx
@@ -0,0 +1,34 @@
+import { Navigate } from "react-router";
+import { useEffect, useState } from "react";
+import { getAccessToken } from "../services/pkceAuth";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function RequireAuth({ children }: Props) {
+  const [loading, setLoading] = useState(true);
+  const [authorized, setAuthorized] = useState(false);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        await getAccessToken(); // Intenta refrescar si hace falta
+        setAuthorized(true);
+      } catch (error) {
+        console.error("Token refresh failed", error);
+        setAuthorized(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (loading) return <p>Checking auth...</p>;
+
+  if (!authorized) return <Navigate to="/login" replace />;
+
+  return <>{children}</>;
+}
