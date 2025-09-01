d8b38ca9d53fafd23c6d1ee5888e5f86269f6fc1
TheScientist137 <thescientist137@outlook.es>
2025-09-01 17:03:06 +0200

    added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Callback from "./pages/Callback.tsx";
 import RequireAuth from "./components/RequireAuth.tsx";
 import Home from "./pages/Home.tsx";
 import Dashboard from "./components/Dashboard.tsx";
-import Playlists from "./pages/Playlists.tsx";
 import SearchResults from "./pages/SearchResults.tsx";
+import UserPlaylist from "./pages/UserPlaylist.tsx";
+import UserLibrary from "./pages/UserLibrary.tsx";
 import Album from "./pages/Album.tsx";
 import Artist from "./pages/Artist.tsx";
 import Playlist from "./pages/Playlist.tsx";
+import CreatePlaylist from "./pages/CreatePlaylist.tsx";
 
 function App() {
   return (
@@ -24,11 +26,13 @@ function App() {
         }
       >
         <Route index element={<Home />} />
-        <Route path="playlists" element={<Playlists />} />
+        <Route path="playlists" element={<UserLibrary />} />
         <Route path="search" element={<SearchResults />} />
         <Route path="album/:id" element={<Album />} />
         <Route path="artist/:id" element={<Artist />} />
-        <Route path="playlist/:id" element={<Playlist />} />
+        <Route path="playlist/:id" element={<UserPlaylist />} />
+        <Route path="playlist/public/:id" element={<Playlist />} />
+        <Route path="playlist/new" element={<CreatePlaylist />} />
       </Route>
     </Routes>
   );
