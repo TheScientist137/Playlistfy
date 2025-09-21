25eac43439b8952b67824a1b939d34dbe42d674f
TheScientist137 <thescientist137@outlook.es>
2025-09-21 17:52:08 +0200

    created fileToBase64 util function

---
+++ src/utils/utils.ts
@@ -0,0 +1,16 @@
+// Convert file to base64 string
+export const fileToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if (typeof reader.result === "string") {
+        const base64 = reader.result.split(",")[1];
+        resolve(base64);
+      } else {
+        reject("Failed to convert file");
+      }
+    };
+    reader.onerror = (error) => reject(error);
+  });
+};
